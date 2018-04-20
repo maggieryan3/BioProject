4ed17a6a5a5f5106a574d177d4940693ee893969
Adrian Low <lowa19@up.edu>
2018-04-20 09:19:24 -0700

    anova test on all

--- FR.py
+++ FR.py
@@ -1,6 +1,7 @@
 #imports
 import pandas as pd
 import xlrd
+from scipy import stats
 
 ##  Non-Mammal:
 ##      1. How do the 3 site types of degraded, restored, and intact differ in terms of vegetation,
@@ -15,11 +16,50 @@ myfile = pd.ExcelFile('Field_Restoration.xlsx')
 mydata = pd.ExcelFile(myfile)
 
 #printing out all the sheet names in the excel file
-print(mydata.sheet_names)
+#print(mydata.sheet_names)
 
 #extracting data from the first sheet as a dataframe
+'''
 dataframe = myfile.parse('Mammals')
 print(dataframe)
+'''
 
 dataframe = myfile.parse('NonMammal')
-print(dataframe)
+
+def runTestOnAll(dataFrame):
+    categories = list(dataframe.columns.values)
+    for cat in categories:
+        if cat != "site.type" and cat != "site" and cat != "station":
+            testResults = anovaTest(dataFrame, cat)
+            print(cat + ": F-value = " + str(testResults.statistic) + " P-value = " + str(testResults.pvalue))
+    return
+
+def anovaTest(dataFrame, category):
+
+    reducedDF = dataFrame[['site.type', category]]
+
+    dArray = []
+    rArray = []
+    iArray = []
+    for index, row in reducedDF.iterrows():
+        site = row['site.type']
+        val = row[category]
+        if site== 'D':
+            dArray.append(val)
+        elif site == 'R':
+            rArray.append(val)
+        elif site == 'I':
+                iArray.append(val)
+        else:
+            print("ERROR\n")
+
+    anova = stats.f_oneway(dArray, rArray, iArray)
+    return anova
+
+#function calls
+runTestOnAll(dataframe)
+#n = anovaTest(dataframe, "av.bac")
+
+
+
+
