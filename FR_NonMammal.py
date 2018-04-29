f9eacecd058ab156ad44e57dce30002bd39e9346
Adrian Low <lowa19@up.edu>
2018-04-29 11:35:20 -0700

    update

--- FR_NonMammal.py
+++ FR_NonMammal.py
@@ -30,8 +30,14 @@ def runTestOnAll(dataFrame):
     passCount = 0
     for cat in reducedCategories:
         passCount += tTest(dataFrame, cat)
-    print("______________TTEST COMPLETE___________________________")
-    print("Number of tests passed: " + passCount)
+    print("Number of tests passed: " + str(passCount))
+    print("________TTEST COMPLETE, CORRELATION TEST STARTING_______")
+    nematodes = dataFrame[['nem.total.av']]
+    for cat in categories:
+        if cat != "site.type" and cat != "site" and cat != "station" and cat != "nem.total.av":
+            correlationTest(nematodes, cat, dataFrame)
+    print("The rest of the variables were not statistically significant and/ or had a weak correlation")
+    print("________________CORRELATION TEST COMPLETE_______________")
     return
 
 def tchecker(type1, type2, pval, cat):
@@ -61,13 +67,13 @@ def tTest(dataFrame, category):
             iArray.append(val)
         else:
             print("ERROR\n")
-            
+    temp = 0
     t1 = stats.ttest_ind(dArray, iArray, equal_var = False)
-    temp = tchecker("degrated", "intact", t1.pvalue, category)
+    temp += tchecker("degrated", "intact", t1.pvalue, category)
     t2 = stats.ttest_ind(dArray, rArray, equal_var = False)
-    temp = tchecker("degrated", "restored", t2.pvalue, category)
+    temp += tchecker("degrated", "restored", t2.pvalue, category)
     t3 = stats.ttest_ind(iArray, rArray, equal_var = False)
-    temp = tchecker("intact", "restored", t3.pvalue, category)
+    temp += tchecker("intact", "restored", t3.pvalue, category)
 
     return temp
 
@@ -94,11 +100,33 @@ def anovaTest(dataFrame, category):
 
     anova = stats.f_oneway(dArray, rArray, iArray)
     if anova.pvalue < strength:
-        print("PASS - Since results for " + category + " proved statistically significant with a p-value of " + str(anova.pvalue) + " we will continue with a t-test...")
+        print("PASS - " + category + " P-Value= " + str(anova.pvalue) + " we will continue with a t-test...")
         return category
     else:
-        print("FAIL - Since the p-value: " + str(anova.pvalue)+ " is greater than " + str(strength) + " we cannot say there is statistical significance for " + category)
+        print("FAIL - " + category + "P-Value= " + str(anova.pvalue))
         return None
 
-#function calls
+'''
+This function tests the correlation between nematodes and other variables
+in order to see how nematodes cluster, using Spearman's correlation.
+'''
+def correlationTest(list, category, dataFrame):
+    reducedDF = dataFrame[[category]]
+    spearman = stats.spearmanr(list, reducedDF)
+    if spearman.pvalue <= 0.05: #only consider statistically significant ones
+        if spearman.correlation > .79:
+            print("There is a VERY STRONG POSITIVE correlation between the number of nematodes and " + category + " Correlation = " + str(spearman.correlation))
+        elif spearman.correlation > .59:
+            print("There is a STRONG POSITIVE correlation between the number of nematodes and " + category + " Correlation = " + str(spearman.correlation))
+        elif spearman.correlation > .39:
+            print("There is a MODERATE POSITIVE correlation between the number of nematodes and " + category + " Correlation = " + str(spearman.correlation))
+        elif spearman.correlation < -.79:
+            print("There is a VERY STRONG NEGATIVE correlation between the number of nematodes and " + category + " Correlation = " + str(spearman.correlation))
+        elif spearman.correlation < -.59:
+            print("There is a STRONG NEGATIVE correlation between the number of nematodes and " + category + " Correlation = " + str(spearman.correlation))
+        elif spearman.correlation < -.39:
+            print("There is a MODERATE NEGATIVE correlation between the number of nematodes and " + category + " Correlation = " + str(spearman.correlation))
+    return
+
+#MAIN
 runTestOnAll(dataframe)
